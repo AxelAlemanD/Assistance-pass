bd43491f554111b0f060aab90403447cfd3bee5a
AxelAlemanD <axelad38@gmail.com>
2022-06-04 10:38:05 -0500

    add: Project structure

---
+++ app/src/main/java/com/upv/pm_2022/iti_27849_u1_equipo_01/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.upv.pm_2022.iti_27849_u1_equipo_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/upv/pm_2022/iti_27849_u1_equipo_01/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.upv.pm_2022.iti_27849_u1_equipo_01
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
