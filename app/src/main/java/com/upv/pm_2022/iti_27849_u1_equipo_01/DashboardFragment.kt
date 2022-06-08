66948e200ecfb6af344663e422a0b7ea9a832cdd
AxelAlemanD <axelad38@gmail.com>
2022-06-07 19:24:31 -0500

    add: Temporary options to ListView

--- app/src/main/java/com/upv/pm_2022/iti_27849_u1_equipo_01/DashboardFragment.kt
+++ app/src/main/java/com/upv/pm_2022/iti_27849_u1_equipo_01/DashboardFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.Spinner
 
 /**
@@ -17,6 +18,9 @@ class DashboardFragment : Fragment() {
 
     lateinit var spGroups       : Spinner
     lateinit var adapterGroups  : ArrayAdapter<CharSequence>
+    lateinit var listView       : ListView
+    var listStudents     : MutableList<String> = mutableListOf()
+    lateinit var adapterStudents  : ArrayAdapter<CharSequence>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,11 +30,29 @@ class DashboardFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
         spGroups = view.findViewById(R.id.groupsSpinner)
+        listView = view.findViewById(R.id.listView)
 
+
+        // Load test data into spinner
         adapterGroups = ArrayAdapter.createFromResource(requireContext(), R.array.groups, android.R.layout.simple_spinner_item);
         adapterGroups.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        // Bind adapter with operator spinner
-        spGroups.setAdapter(adapterGroups);
+        spGroups.setAdapter(adapterGroups); // Bind adapter with operator spinner
+
+
+        // Load test data into ListView
+        listStudents.add("Axel Issai Alemán Delgado")
+        listStudents.add("José Carlos Avalos Ruiz")
+        listStudents.add("José Manuel Rodriguez Garcia")
+        listStudents.add("Juan de Dios Nava Gallardo")
+        listStudents.add("Orlando Samuel Martinez Dorantes")
+
+        adapterStudents = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1,
+            listStudents as List<CharSequence>
+        )
+        listView.adapter = adapterStudents
+
+
+
 
         return view
     }
