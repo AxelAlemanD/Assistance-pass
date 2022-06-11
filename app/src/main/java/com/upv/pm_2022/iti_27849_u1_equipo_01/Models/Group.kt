f2d9c8e70593b0aaea7ff4286f35fc094c549605
AxelAlemanD <axelad38@gmail.com>
2022-06-11 10:14:12 -0500

    add: Group model

---
+++ app/src/main/java/com/upv/pm_2022/iti_27849_u1_equipo_01/Models/Group.kt
@@ -0,0 +1,28 @@
+package com.upv.pm_2022.iti_27849_u1_equipo_01.Models
+
+import android.content.ContentValues
+import com.upv.pm_2022.iti_27849_u1_equipo_01.Contracts.GroupContract
+
+class Group(var id: Int? = null, var name: String? = null){
+
+    /**
+     * Convierte la información de la instancia a ContentValues
+     * @return ContentValues valores de la instancia
+     */
+    fun toContentValues(): ContentValues {
+        // Create a new map of values, where column names are the keys
+        val values = ContentValues().apply {
+            put(GroupContract.FeedGroup.COLUMN_NAME_NAME, name)
+        }
+
+        return values
+    }
+
+    /**
+     * Parsea la instancia a String
+     * @return String
+     */
+    override fun toString() : String{
+        return "ID: $id \t NAME: $name"
+    }
+}
