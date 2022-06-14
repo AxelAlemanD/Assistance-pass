9e078bbabb3e8ce61edfdcafb0ad2b28b39c0622
AxelAlemanD <axelad38@gmail.com>
2022-06-13 23:06:52 -0500

    add: New tables

--- app/src/main/java/com/upv/pm_2022/iti_27849_u1_equipo_01/DbHelper.kt
+++ app/src/main/java/com/upv/pm_2022/iti_27849_u1_equipo_01/DbHelper.kt
@@ -3,7 +3,9 @@ package com.upv.pm_2022.iti_27849_u1_equipo_01
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.upv.pm_2022.iti_27849_u1_equipo_01.Contracts.ASSISTANCES
 import com.upv.pm_2022.iti_27849_u1_equipo_01.Contracts.GROUPS
+import com.upv.pm_2022.iti_27849_u1_equipo_01.Contracts.GROUPS_STUDENTS
 import com.upv.pm_2022.iti_27849_u1_equipo_01.Contracts.STUDENTS
 
 class DbHelper (context: Context) : SQLiteOpenHelper(context, "assistancePass.db", null, 1) {
@@ -11,11 +13,15 @@ class DbHelper (context: Context) : SQLiteOpenHelper(context, "assistancePass.db
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL(GROUPS.SQL_CREATE_GROUPS_TABLE)
         db.execSQL(STUDENTS.SQL_CREATE_STUDENTS_TABLE)
+        db.execSQL(GROUPS_STUDENTS.SQL_CREATE_GROUPS_STUDENTS_TABLE)
+        db.execSQL(ASSISTANCES.SQL_CREATE_ASISSTANCES_TABLE)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         db.execSQL(GROUPS.SQL_DELETE_GROUPS_TABLE)
         db.execSQL(STUDENTS.SQL_DELETE_STUDENTS_TABLE)
+        db.execSQL(GROUPS_STUDENTS.SQL_DELETE_GROUPS_STUDENTS_TABLE)
+        db.execSQL(ASSISTANCES.SQL_DELETE_ASISSTANCES_TABLE)
         onCreate(db)
     }
 }
