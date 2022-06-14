7a7f2b53aca48d1e4492f292a2a0e3f57575694e
AxelAlemanD <axelad38@gmail.com>
2022-06-13 23:05:56 -0500

    fix: Import required objects

--- app/src/main/java/com/upv/pm_2022/iti_27849_u1_equipo_01/Models/Group.kt
+++ app/src/main/java/com/upv/pm_2022/iti_27849_u1_equipo_01/Models/Group.kt
@@ -1,7 +1,9 @@
 package com.upv.pm_2022.iti_27849_u1_equipo_01.Models
 
 import android.content.ContentValues
+import com.upv.pm_2022.iti_27849_u1_equipo_01.Contracts.ASSISTANCES
 import com.upv.pm_2022.iti_27849_u1_equipo_01.Contracts.GROUPS
+import com.upv.pm_2022.iti_27849_u1_equipo_01.Contracts.GROUPS_STUDENTS
 import java.io.Serializable
 
 class Group(var id: Int? = null, var name: String? = null) : Serializable {
