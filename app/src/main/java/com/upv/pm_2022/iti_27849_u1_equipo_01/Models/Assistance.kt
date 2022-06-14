8799ef6fa49e217374be4111fb6305a8ab517e47
AxelAlemanD <axelad38@gmail.com>
2022-06-13 23:03:35 -0500

    add: Assistance model

---
+++ app/src/main/java/com/upv/pm_2022/iti_27849_u1_equipo_01/Models/Assistance.kt
@@ -0,0 +1,24 @@
+package com.upv.pm_2022.iti_27849_u1_equipo_01.Models
+
+import android.content.ContentValues
+import com.upv.pm_2022.iti_27849_u1_equipo_01.Contracts.ASSISTANCES
+import com.upv.pm_2022.iti_27849_u1_equipo_01.Contracts.GROUPS
+import com.upv.pm_2022.iti_27849_u1_equipo_01.Contracts.GROUPS_STUDENTS
+import java.io.Serializable
+
+class Assistance(var id: Int? = null, var date: String, var is_late: Int, var group_id: Int, var student_id: Int){
+
+    /**
+     * Convert attributes to ContentValues
+     * @return ContentValues
+     */
+    fun toContentValues(): ContentValues {
+        val values = ContentValues().apply {
+            put(ASSISTANCES.COLUMN_NAME_DATE, date)
+            put(ASSISTANCES.COLUMN_NAME_IS_LATE, is_late)
+            put(ASSISTANCES.COLUMN_NAME_GROUP_ID, group_id)
+            put(ASSISTANCES.COLUMN_NAME_STUDENT_ID, student_id)
+        }
+        return values
+    }
+}
